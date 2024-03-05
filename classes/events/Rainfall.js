71c318f3fdb852e9f5ec07dd5392a13bd5d210d2
paytondev <81662879+paytontech@users.noreply.github.com>
2024-03-05 10:59:25 -0500

    rain working

--- classes/events/Rainfall.js
+++ classes/events/Rainfall.js
@@ -21,10 +21,11 @@ class Rainfall extends Event {
         this.rain = [];
     }
     activate(gameWorld) {
-        this.umbrella = new Umbrella(gameWorld);
-        for (let i = 0; i < 500; i++) {
+
+        for (let i = 0; i < 50; i++) {
             this.rain.push(new Rain());
         }
+        this.umbrella = new Umbrella(gameWorld);
     }
 }
 
@@ -32,11 +33,8 @@ class Umbrella extends DynamicCreature {
     constructor(gameWorld) {
         super("umbrella", createVector(random(0 + gameWorld.gameObjects[0].sprite.x, width + gameWorld.gameObjects[0].sprite.x), width / 4), createVector(0, 0), false, false, true, 1, new IdleState());
         this.sprite.remove();
-        this.sprite = new Sprite(this.pos.x, this.pos.y);
-        this.sprite.collider = "k";
-        this.sprite.w = 200;
-        this.sprite.h = 10;
-        this.sprite.debug = true;
+        this.sprite = new Sprite(this.pos.x, this.pos.y, 200, 10, "k");
+        // this.sprite.debug = true;
     }
     update(gameWorld, rain) {
         for (let rainEntity of rain) {
@@ -52,15 +50,25 @@ class Rain extends DynamicCreature {
         super("rain", createVector(random(0 + gameWorld.gameObjects[0].sprite.x, width + gameWorld.gameObjects[0].sprite.x), -height - random(0, height)), createVector(0, 0), true, false, false, 1, new IdleState());
         this.sprite.remove();
         this.sprite = new Sprite(this.pos.x, this.pos.y);
-        this.sprite.collider = "k";
+        this.sprite.collider = "d";
         this.sprite.h = 50;
         this.sprite.w = 10;
-        this.sprite.debug = true;
+        // this.sprite.debug = true;
+        this.sprite.mass = 0.25;
     }
     update(gameWorld) {
-        this.sprite.pos.y += 2;
+        this.sprite.applyForceScaled(0, -9);
         if (gameWorld.gameObjects[0].sprite.collides(this.sprite)) {
             gameWorld.gameObjects[0].kill();
         }
+        if (this.sprite.y == height / 1.5) {
+            this.sprite.remove();
+        }
+        if (this.sprite.collides(gameWorldBG.floor)) {
+            this.sprite.remove();
+        }
+        // if (this.sprite.velocity.y == 0) {
+        //     this.sprite.remove();
+        // }
     }
 }
