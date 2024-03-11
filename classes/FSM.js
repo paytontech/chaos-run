5d3ee97822af84e63a55e37e4e69222e476b4a83
paytondev <81662879+paytontech@users.noreply.github.com>
2024-03-11 12:24:08 -0400

    fmt

--- classes/FSM.js
+++ classes/FSM.js
@@ -1,46 +1,45 @@
 class FSM {
-    constructor() {
-      this.stack = [];
-      this.currentState = null;
-      this.nextState = null;
+  constructor() {
+    this.stack = [];
+    this.currentState = null;
+    this.nextState = null;
+  }
+
+  getCurrentState() {
+    if (this.stack.length > 0) {
+      return this.stack[this.stack.length - 1];
+    } else {
+      return null;
     }
-  
-    getCurrentState() {
-      if (this.stack.length > 0) {
-        return this.stack[this.stack.length - 1];
-      } else {
-        return null;
+  }
+  popState() {
+    this.stack.pop();
+  }
+  pushState(state) {
+    if (state != null && state != this.getCurrentState()) {
+      this.currentState.exit();
+
+      if (this.currentState.popable) {
+        this.popState();
       }
-    }
-    popState() {
-      this.stack.pop();
-    }
-    pushState(state) {
-      if (state != null && state != this.getCurrentState()) {
-        this.currentState.exit();
-  
-        if (this.currentState.popable) {
-          this.popState();
-        }
-        if (this.getCurrentState().constructor != state.constructor) {
-          this.stack.push(state);
-        }
-        this.currentState = this.getCurrentState();
-        this.currentState.enter();
+      if (this.getCurrentState().constructor != state.constructor) {
+        this.stack.push(state);
       }
-    }
-    update() {
-      this.nextState = this.currentState.handleInput();
-      this.pushState(this.nextState);
-      //
-      this.nextState = this.currentState.update();
-      this.pushState(this.nextState);
-    }
-    setInitialState(state) {
-      this.stack = [];
-      this.stack.push(state);
       this.currentState = this.getCurrentState();
       this.currentState.enter();
     }
   }
-  
+  update() {
+    this.nextState = this.currentState.handleInput();
+    this.pushState(this.nextState);
+    //
+    this.nextState = this.currentState.update();
+    this.pushState(this.nextState);
+  }
+  setInitialState(state) {
+    this.stack = [];
+    this.stack.push(state);
+    this.currentState = this.getCurrentState();
+    this.currentState.enter();
+  }
+}
